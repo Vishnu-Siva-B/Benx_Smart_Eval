f56477c814b19d641b4b6e4859327d999b41b4dc
Vamsi KS <ksvamsi96@gmail.com>
2025-03-15 03:25:45 +0530

    to prod

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from benx_1 import create_app
-# from waitress import serve
+from waitress import serve
 
 app = create_app()
 
 if __name__ == "__main__":
-    # serve(app, host="0.0.0.0", port=8000)
-    app.run(debug=1)
+    serve(app, host="0.0.0.0", port=8000)
+    # app.run(debug=1)
